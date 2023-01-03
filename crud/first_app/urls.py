762baa93abe33370eff123d6040b06a15509c102
muhammed-sami-chowdhury <samiulchowdhury98@gmail.com>
2023-01-03 16:48:26 +0600

    read & update

--- crud/first_app/urls.py
+++ crud/first_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import index, studet_form
+from .views import index, studet_form, StudentInfo, StudentUpdate
 
 app_name = 'first_app'
 
 urlpatterns = [
     path('', index, name='index'),
     path('student_form', studet_form, name='student_form'),
+    path('student_info/<int:student_id>/', StudentInfo, name='student_info'),
+    path('student_update/<int:student_id>', StudentUpdate, name='student_update'),
     ]
--- crud/first_app/views.py
+++ crud/first_app/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
 from .form import StudentForm
+from .models import Student
 
 def index(request):
-    diction = {'title':'index'}
+    student_list = Student.objects.order_by('first_name')
+    diction = {'title': "index", 'student_list': student_list}
     return render(request, 'first_app/index.html', context=diction)
 
 
 def studet_form(request):
     form = StudentForm()
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+
     diction = {'title': 'student_form', 'student_form':form}
     return render(request, 'first_app/student_form.html', context=diction)
+
+
+
+def StudentInfo(request, student_id):
+    student_info = Student.objects.get(pk=student_id)
+    diction = {'student_info':student_info}
+    return render(request, 'first_app/student_info.html', context=diction)
+
+
+def StudentUpdate(request):
+    diction = {}
+    return render(request, 'first_app/student_update.html', context=diction)
